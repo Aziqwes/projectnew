c7562526f8fc559de3bb7499c4a634c2868ca5fd
Aziqwes <67837425+Aziqwes@users.noreply.github.com>
2020-12-13 19:53:21 +0300

    Work on css or html and js

---
+++ script.js
@@ -0,0 +1,12 @@
+function psiphontest(){
+    alert("IP:45.139.111.158 Port:8000 Login:yp9cMq Password:mamTqE");
+}
+function ssrtest(){
+    alert("Этот раздел в данный момент не активен")
+}
+function ss(){
+    alert("ss://YWVzLTI1Ni1nY206eDIzWjRMR2tHRGtUaFo5S2F6NERVUlFwQDM4LjEzMi4xMTIuMTAxOjQwMDkz#1")
+}
+
+
+
